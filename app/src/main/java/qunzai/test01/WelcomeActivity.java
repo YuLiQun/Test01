20f315c5a5e4ad3c22e958fc1698de920f7b31a5
邢明达 <cankered2008@163.com>
2016-11-19 15:02:56 +0800

    更新

---
+++ app/src/main/java/qunzai/test01/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package qunzai.test01;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/11/19.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_activity);
+    }
+}
