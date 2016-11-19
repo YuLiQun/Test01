13e81b775f5b4b09785c162e8048e9bc20d2ac53
YuLiQun <1347726311@qq.com>
2016-11-19 15:01:10 +0800

    测试啊。。群仔

---
+++ app/src/main/java/qunzai/test01/Bean.java
@@ -0,0 +1,18 @@
+package qunzai.test01;
+
+/**
+ * Created by dllo on 16/11/19.
+ */
+
+public class Bean {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
